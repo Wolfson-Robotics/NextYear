47315d947c6d850019d80d29a0c851d58199f47b
HarrisonFld <fieldharrison@proton.me>
2025-08-20 20:43:19 -0400

    Added IMUTest class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/IMUTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.RobotBase;
+
+public class IMUTest extends RobotBase {
+    //see: https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html
+    IMU imu = hardwareMap.get(IMU.class, "imu");
+
+    @Override
+    public void init() {
+        imu.initialize(new IMU.Parameters(
+            new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+            ))
+        );
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.y) {
+            telemetry.addLine("Resetting Yaw");
+            imu.resetYaw();
+        }
+
+        // Retrieve Rotational Angles and Velocities
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
+
+        telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+        telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
+        telemetry.addData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
+        telemetry.addData("Yaw (Z) velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate);
+        telemetry.addData("Pitch (X) velocity", "%.2f Deg/Sec", angularVelocity.xRotationRate);
+        telemetry.addData("Roll (Y) velocity", "%.2f Deg/Sec", angularVelocity.yRotationRate);
+        telemetry.update();
+    }
+
+}
